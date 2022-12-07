870dff5f9ea66bfd6c98da0cf98bfa7bcb181d0f
bonnyAL99 <giuseppe.bonanno9@gmail.com>
2022-12-07 14:58:52 +0100

    adding DB tracking image

--- glassy/Assets/GPSLocation.cs
+++ glassy/Assets/GPSLocation.cs
@@ -71,28 +71,8 @@ public class GPSLocation : MonoBehaviour
   
     /*private void UpdateGPSData()
     {
-        Coordinate[] cordinate=new Coordinate[5];
-        cordinate[0].latitude=45.484835f;   //SALVIA RUSSA
-        cordinate[0].longitude=9.193702f;
-        cordinate[1].latitude=45.484531f;   //SALICE PIANGENTE 
-        cordinate[1].longitude=9.191907f;
-        cordinate[2].latitude=45.484827f;   //FRASSINO
-        cordinate[2].longitude=9.192582f;
-        cordinate[3].latitude=45.483627f;   //PAPAVERO
-        cordinate[3].longitude=9.193029f;
-        cordinate[4].latitude=45.484933f;   //IRIS SIBIRICA
-        cordinate[4].longitude=9.193889f;
-        
         if (Input.location.status == LocationServiceStatus.Running)
-        {d
-            GPSStatus.text = "Running";
-            for(int i=0;i<5;i++){
-                if(cordinate[i].latitude<45.485366)
-            }
-
-
-            latitudeValue = Input.location.lastData.latitude;
-            longitudeValue = Input.location.lastData.longitude;
+        {
            
         }
         else if(Input.location.status == LocationServiceStatus.Failed)
